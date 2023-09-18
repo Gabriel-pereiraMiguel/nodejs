1055937e239402aba7480b8037be84276ac55fe3
Gabriel Miguel <109819028+Gabriel-pereiraMiguel@users.noreply.github.com>
2023-09-18 15:03:54 -0300

    Update volume.js "simbol adicionado"

--- volume.js
+++ volume.js
@@ -3,6 +3,6 @@ const prompt = require('prompt-sync')();
 const pi = 3.14159;
 var raio = prompt('Digite o raio: ');
 var altura = prompt('digite a altura: ')
-var volume = pi * raio ^ 2 * altura;
+var volume = pi * raio ** 2 * altura;
 
 console.log(volume)
